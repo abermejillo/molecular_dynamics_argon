8cd5e9698ec7d6bb9b0da873f8287bb623ffe5b7
abermejillo <a.bermejilloseco@student.tudelft.nl>
2022-02-09 12:00:58 +0100

    Add force calculation

--- main.py
+++ main.py
@@ -30,4 +30,23 @@ MASS = 6.6335209E-26 # Argon particle mass
 pos = L*np.random.rand(N, 2)
 vel = np.sqrt(KB*T/MASS)*np.random.rand(N, 2)
 
-# Force calculation using the Lennard-Jones potential
+# Compute relative positions and distances
+rel_pos = np.zeros((N,N,2))
+rel_dist = np.zeros((N,N))
+
+for i in range(N):
+    for j in range(i):
+        rel_pos[i,j] = pos[i] - pos[j]
+        rel_dist[i,j] = np.linalg.norm(rel_pos[i,j])
+
+# Force calculation using the Lennard-Jones potential       
+force = np.zeros((N,N,2)) # Forces between pairs
+
+for i in range(N):
+    for j in range(i):
+        force[i,j] = 24*EPSILON*rel_pos[i,j] * (2*SIGMA**12/rel_dist[i,j]**14 - SIGMA**6/rel_dist[i,j]**8)
+
+force = force + force.transpose((1,0,2)) # Fill upper triangle of the matrix 
+
+# Total force exerted on each particle
+total_force = force.sum(1) 
