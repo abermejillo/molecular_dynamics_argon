23bd7ff08309bd647012e0aea8de2b535aa59c2b
abermejillo <a.bermejilloseco@student.tudelft.nl>
2022-02-12 18:23:21 +0100

    Rewrite using simulate.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import simulate as sim
 
 """
 Initial attempt: 3 Ar particles in 2D
@@ -18,100 +19,33 @@ UNITS: in International System
 """
 
 # Input parameters
-N = 8 # number of particles
+N = 10 # number of particles
 L = 1E-6 # box length
 T = 300 # temperature
-At = 1E-11 # time-step
+time_step = 1E-11 # time-step
 run_time = 1E-9 # run time of the simulation
-
-# Global constants
-KB = 1.3806E-23 # Boltzmann constant 
-SIGMA = 3.405E-10 # parameter of LJ potential
-EPSILON = 119.8*KB # parameter of LJ potential
-MASS = 6.6335209E-26 # Argon particle mass
-
-# Functions to compute the energy of the system
-def potential_energy(rel_dist): 
-    """Potential energy function"""
-    mask = np.array(1 - np.identity(rel_dist.shape[0]), dtype=bool)[:,:,np.newaxis] # mask for skipping diagonal terms
-    pot_energy = 4*EPSILON*(SIGMA**12/rel_dist**12-SIGMA**6/rel_dist**6)
-    pot_energy = np.sum(pot_energy, where=mask)/2 # The diagonal terms are skipped (no self-interaction)
-    return pot_energy
-
-def kinetic_energy(vel):
-    """Kinetic energy function"""
-    return 0.5*MASS*(vel**2).sum()
-
-def total_energy(rel_dist, vel): 
-    """Returns total energy of the system"""
-    kin_energy = kinetic_energy(vel)
-    pot_energy = potential_energy(rel_dist)
-    return kin_energy + pot_energy
+Niter = 100 # number of times the simulation is run
 
 # Velocity and Position initialization (uniformly random)
 pos = L*np.random.rand(N, 2)
-vel = np.sqrt(KB*T/MASS) - 2*np.sqrt(KB*T/MASS)*np.random.rand(N, 2)
+vel = np.sqrt(sim.KB*T/sim.MASS) - 2*np.sqrt(sim.KB*T/sim.MASS)*np.random.rand(N, 2)
 
 # Total energy storage vector
 tot_energy=[]
 
-# Euler method
-for k, t in enumerate(np.arange(0, run_time + At, At)):
+# Simulation
+# Check that the energy is conserved 
 
-    # Compute relative positions
-    pos1 = np.repeat(pos[:, np.newaxis, :], N, axis=1)
-    pos2 = np.repeat(pos[np.newaxis, :, :], N, axis=0)
-    rel_pos = pos1 - pos2
+for t in range(Niter):
+    pos, vel = sim.simulate(pos,vel,int(run_time/time_step),time_step,L)
+    rel_pos, rel_dist = sim.atomic_distances(pos,L)
+    tot_energy += [sim.total_energy(rel_pos,vel)]
 
-    # minimum image convention (for the periodic boundary conditions)
-    wrong_pairs = np.where(np.abs(rel_pos) > L/2)
-    rel_pos[wrong_pairs] = rel_pos[wrong_pairs] - np.sign(rel_pos[wrong_pairs])*L
-
-    # Compute relative distance
-    rel_dist = np.linalg.norm(rel_pos, axis=2) # axis 2 contains the cartesian coordinates
-    rel_dist = rel_dist[:,:,np.newaxis] # add axis for LJ force calculation
-    rel_dist[np.diag_indices(N)] = 1 # avoiding division by zero in the diagonal when calculating LJ force
-
-    # plotting for checking the interactions
-    if True:
-        for i in range(N):
-            # plot normal box and its eight neighbours
-            plt.plot(pos[i,0], pos[i,1], "r.")
-            plt.plot(pos[i,0]+L, pos[i,1], "r.")
-            plt.plot(pos[i,0], pos[i,1]+L, "r.")
-            plt.plot(pos[i,0]+L, pos[i,1]+L, "r.")
-            plt.plot(pos[i,0]-L, pos[i,1], "r.")
-            plt.plot(pos[i,0], pos[i,1]-L, "r.")
-            plt.plot(pos[i,0]-L, pos[i,1]-L, "r.")
-            plt.plot(pos[i,0]-L, pos[i,1]+L, "r.")
-            plt.plot(pos[i,0]+L, pos[i,1]-L, "r.")
-
-            for j in range(i):
-                plt.plot([pos[j,0], pos[j,0] + rel_pos[i,j,0]], [pos[j,1], pos[j,1] + rel_pos[i,j,1]], "b--") # correction for BC
-
-        plt.plot([0,0,L,L,0],[0,L,L,0,0], "g-") # plot square for normal box
-        plt.xlim(-L/2, 3*L/2)
-        plt.ylim(-L/2, 3*L/2)
-        plt.savefig("img{:05d}.png".format(k)) # convert -delay 5 img*.png movie.gif
-        plt.cla()
-
-    # Force calculation using the Lennard-Jones potential  
-    force = 24*EPSILON*rel_pos*(2*SIGMA**12/rel_dist**14 - SIGMA**6/rel_dist**8)
-
-    # Total force exerted on each particle
-    total_force = np.sum(force, axis=1)
-
-    # Update velocities and positions
-    pos = pos + vel*At
-    vel = vel + total_force*At/MASS
-
-    # Check periodic boundary conditions
-    pos = pos - np.floor(pos/L)*L
-
-    # Save the total energy
-    tot_energy += [total_energy(rel_dist, vel)]
-
+print(tot_energy[1:5])
 
 # Plotting the energy
-plt.plot(np.linspace(0, run_time, len(tot_energy)),tot_energy)
+ax = plt.axes()
+plt.plot(np.linspace(0, Niter, len(tot_energy)),tot_energy)
+plt.axis([0, 100, 1E-21, 0.2E-19])
 plt.show()
+
