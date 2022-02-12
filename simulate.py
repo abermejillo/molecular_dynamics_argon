cd65f120aef3a9b7e44944fb6d4861ae00cba121
Marc Serra Peralta <marcserraperalta@gmail.com>
2022-02-12 17:35:44 +0100

    Update documentation

--- simulate.py
+++ simulate.py
@@ -1,33 +1,24 @@
-"""
-This is a suggestion for structuring your simulation code properly.
-However, it is not set in stone. You may modify it if you feel like
-you have a good reason to do so.
-"""
-
 import numpy as np
 
-# Global constants
-KB = 1.3806E-23 # Boltzmann constant 
-SIGMA = 3.405E-10 # parameter of LJ potential
-EPSILON = 119.8*KB # parameter of LJ potential
-MASS = 6.6335209E-26 # Argon particle mass
+KB = 1.3806E-23 # Boltzmann constant in SI
+SIGMA = 3.405E-10 # parameter of LJ potential for Argon atoms in SI
+EPSILON = 119.8*KB # parameter of LJ potential for Argon atoms in SI
+MASS = 6.6335209E-26 # Argon particle mass in SI
 
 
-# Functions for simulation
 def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
     """
-    Molecular dynamics simulation using the Euler or Verlet's algorithms
-    to integrate the equations of motion. Calculates energies and other
-    observables at each timestep.
+    Molecular dynamics simulation using the Euler algorithm
+    to integrate the equations of motion. 
 
     Parameters
     ----------
-    init_pos : np.ndarray Nx2
-        The initial positions of the atoms in Cartesian space
-    init_vel : np.ndarray Nx2
-        The initial velocities of the atoms in Cartesian space
+    init_pos : np.ndarra(N,2)
+        Initial positions of the atoms in Cartesian space
+    init_vel : np.ndarray(N,2)
+        Initial velocities of the atoms in Cartesian space
     num_tsteps : int
-        The total number of simulation steps
+        Total number of simulation steps
     timestep : float
         Duration of a single simulation step
     box_dim : float
@@ -35,27 +26,32 @@ def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
 
     Returns
     -------
-    Any quantities or observables that you wish to study.
+    pos : np.ndarra(N,2)
+        Positions of the atoms in Cartesian space after the simulation
+    vel : np.ndarra(N,2)
+        Velocities of the atoms in Cartesian space after the simulation
     """
 
-    pos = init_pos.copy() # Create a new object with the positions, that will be updated in the iterations
-    vel = init_vel.copy() # Create a new object with the velocities, that will be updated in the iterations
+    pos = init_pos
+    vel = init_vel
     
-    for k in range(num_tsteps+1):
-        simulate_step(pos, vel, timestep, box_dim)
+    for k in range(num_tsteps):
+        pos, vel = simulate_step(pos, vel, timestep, box_dim)
 
-    return
+    return pos, vel
+
 
 def simulate_step(pos, vel, timestep, box_dim):
     """
-    Compute positions and velocities of particles at next time.
+    Computes positions and velocities of particles at next time step
+    using Euler method.
 
     Parameters
     ----------
-    pos : np.ndarray Nx2
-        The positions of the atoms in Cartesian space
-    vel : np.ndarray Nx2
-        The velocities of the atoms in Cartesian space
+    pos : np.ndarray(N,2)
+        Positions of the atoms in Cartesian space
+    vel : np.ndarray(N,2)
+        Velocities of the atoms in Cartesian space
     timestep : float
         Duration of a single simulation step
     box_dim : float
@@ -63,24 +59,24 @@ def simulate_step(pos, vel, timestep, box_dim):
 
     Returns
     -------
+    pos : np.ndarra(N,2)
+        Positions of the atoms in Cartesian space after one time step
+    vel : np.ndarra(N,2)
+        Velocities of the atoms in Cartesian space after one time step
     """
     
-    #Compute relative positions and distances
-    rel_pos_, rel_pos, rel_dist = atomic_distances(pos, box_dim) # rel_pos_ NOT used but WILL be used for plotting
-
-    # Total force exerted on each particle
+    rel_pos, rel_dist = atomic_distances(pos, box_dim)
     total_force = lj_force(rel_pos, rel_dist)
 
-    # Update velocities and positions
+    # Update velocities and positions with Euler method
     pos = pos + vel*timestep
     vel = vel + total_force*timestep/MASS
 
-    # Check periodic boundary conditions
+    # Update positions due to periodic BC
     pos = pos - np.floor(pos/box_dim)*box_dim
 
-    return
+    return pos, vel
     
-
 
 def atomic_distances(pos, box_dim):
     """
@@ -88,14 +84,14 @@ def atomic_distances(pos, box_dim):
 
     parameters
     ----------
-    pos : np.ndarray Nx2
-        The positions of the particles in cartesian space
+    pos : np.ndarray(N,2)
+        Positions of the particles in cartesian space
     box_dim : float
-        The dimension of the simulation box
+        Dimension of the simulation box
 
     returns
     -------
-    rel_pos_ : np.ndarray NxNx2
+    rel_pos : np.ndarray(N,N,2)
         Relative positions of particles without taking into account periodic boundary conditions
         Let pos be the following matrix
             x1 y1
@@ -104,7 +100,7 @@ def atomic_distances(pos, box_dim):
             .  .
             .  .
             .  .
-        Then, rel_pos_ is
+        Then, rel_pos is
             0       x1-x2       x1-x3       ...         |       0       y1-y2       y1-y3       ...
             x2-x1   0           x2-x3       ...         |       y2-y1   0           y2-y3       ...
             x3-x1   x3-x2       0           ...         |       y3-y1   y3-y2       0           ...
@@ -112,11 +108,8 @@ def atomic_distances(pos, box_dim):
             .
             .
         where the separation between x and y relative positions is across the third axis of the matrix.
-    
-    rel_pos : np.ndarray NxNx2
-        Relative positions of particles taking into account periodic boundary conditions
 
-    rel_dist : np.ndarray NxN
+    rel_dist : np.ndarray(N,N)
         The distance between particles
         Each element is sqrt((xi-xj)**2+(yi-yj)**2), where i denotes row and j denotes column
     """
@@ -124,21 +117,17 @@ def atomic_distances(pos, box_dim):
     N = np.shape(pos)[0] # Number of particles
 
     # Compute relative positions
-    pos1 = np.repeat(pos[:, np.newaxis, :], N, axis=1) # NxNx2 matrix
-    pos2 = np.repeat(pos[np.newaxis, :, :], N, axis=0) # NxNx2 matrix
-
+    pos1 = np.repeat(pos[:, np.newaxis, :], N, axis=1)
+    pos2 = np.repeat(pos[np.newaxis, :, :], N, axis=0)
     rel_pos = pos1 - pos2
-
-    rel_pos_ = rel_pos.copy() # for plotting
-
-    # minimum image convention (for the periodic boundary conditions)
+    # check if using the minimum distance between pairs due to BC
     wrong_pairs = np.where(np.abs(rel_pos) > box_dim/2)
     rel_pos[wrong_pairs] = rel_pos[wrong_pairs] - np.sign(rel_pos[wrong_pairs])*box_dim
 
     # Compute relative distance
     rel_dist = np.linalg.norm(rel_pos, axis=2) # axis 2 contains the cartesian coordinates
 
-    return  rel_pos_, rel_pos, rel_dist
+    return  rel_pos, rel_dist
 
 
 def lj_force(rel_pos, rel_dist):
@@ -147,26 +136,22 @@ def lj_force(rel_pos, rel_dist):
 
     Parameters
     ----------
-    rel_pos : np.ndarray NxNx2
+    rel_pos : np.ndarray(N,N,2)
         Relative particle positions as obtained from atomic_distances
-    rel_dist : np.ndarray NxN
+    rel_dist : np.ndarray(N,N)
         Relative particle distances as obtained from atomic_distances
 
     Returns
     -------
-    np.ndarray NxNx2
-        The net force acting on particle i due to all other particles
+    np.ndarray(N,2)
+        Net force acting on each particle due to all other particles
     """
 
-    N = np.shape(rel_pos)[0]
-
     rel_dist = rel_dist[:,:,np.newaxis] # add axis for LJ force calculation (so that it agrees with rel_pos dimensions)
-    rel_dist[np.diag_indices(N)] = 1 # avoiding division by zero in the diagonal when calculating LJ force
+    rel_dist[np.diag_indices(np.shape(rel_dist)[0])] = 1 # avoiding division by zero in the diagonal when calculating LJ force
 
     # Force calculation using the Lennard-Jones potential
     force = 24*EPSILON*rel_pos*(2*SIGMA**12/rel_dist**14 - SIGMA**6/rel_dist**8)
-
-    # Total force exerted on each particle
     total_force = force.sum(1)
 
     return total_force
@@ -198,13 +183,13 @@ def kinetic_energy(vel):
 
     Parameters
     ----------
-    vel: np.ndarray Nx2
-        Velocity of particle
+    vel: np.ndarray(N,2)
+        Velocities of particles
 
     Returns
     -------
     float
-        The total kinetic energy of the system.
+        Total kinetic energy of the system
     """
 
     return 0.5*MASS*(vel**2).sum()
@@ -216,19 +201,21 @@ def potential_energy(rel_dist):
 
     Parameters
     ----------
-    rel_dist : np.ndarray NxN
+    rel_dist : np.ndarray(N,N)
         Relative particle distances as obtained from atomic_distances
 
     Returns
     -------
     float
-        The total potential energy of the system.
+        Total potential energy of the system
     """
 
     mask = np.array(1 - np.identity(rel_dist.shape[0]), dtype=bool)[:,:,np.newaxis] # mask for skipping diagonal terms
     pot_energy = 4*EPSILON*(SIGMA**12/rel_dist**12-SIGMA**6/rel_dist**6)
     pot_energy = np.sum(pot_energy, where=mask)/2 # The diagonal terms are skipped (no self-interaction)
+
     return pot_energy
+
 
 def total_energy(rel_dist, vel): 
     """
@@ -236,20 +223,20 @@ def total_energy(rel_dist, vel):
 
     Parameters
     ----------
-    rel_dist : np.ndarray NxN
+    rel_dist : np.ndarray(N,N)
         Relative particle distances as obtained from atomic_distances
-
-    vel: np.ndarray Nx2
-        Velocity of particle
+    vel: np.ndarray(N,2)
+        Velocities of particles
 
     Returns
     -------
     float
-        The total energy of the system.
+        The total energy of the system
     """
 
     kin_energy = kinetic_energy(vel)
     pot_energy = potential_energy(rel_dist)
+
     return kin_energy + pot_energy
 
 
