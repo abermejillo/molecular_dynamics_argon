066122def1e0b341e5458a4177a4dafa287eccc5
dbedialaunetar <D.bedialaunetarodriguez@student.tudelft.nl>
2022-02-28 11:10:40 +0100

    Add specific heat observable

--- observables.py
+++ observables.py
@@ -43,3 +43,33 @@ def pair_correlation_function(file_name, dr, L, r_max=None):
     print("")
 
     return r, g
+
+def specific_heat(data_file,starting_time_step):
+    """
+    Computes the specific heat per atom of a system.
+
+    Parameters
+    ----------
+    data_file : str
+        Name of the CSV file in which the data is stored
+
+    Returns
+    -------
+    c : float
+        Specific heat per atom of the system.
+    """
+
+    time, pos, vel = sim.load_data(data_file)
+    time = time[starting_time_step:-1]
+    pos = pos[starting_time_step:-1,:,:]
+    vel = vel[starting_time_step:-1,:,:]
+    M = len(time) # Number of time steps
+    N = np.shape(vel)[1] # Number of particles
+    square_of_mean_kin = (sim.kinetic_energy(vel)/M)**2
+    mean_of_square_kin = (((0.5*(vel**2).sum(2)).sum(1))**2).sum()/M
+    
+    r = (mean_of_square_kin - square_of_mean_kin)/square_of_mean_kin # relative fluctuations in kinetic energy
+
+    c = 1.5/(1-3*N*r/2)
+
+    return c
