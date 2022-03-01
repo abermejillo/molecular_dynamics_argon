5d8991861cc090ae9cbbc44fb83a056abf0dea89
abermejillo <a.bermejilloseco@student.tudelft.nl>
2022-03-01 10:30:12 +0100

    Rename some variables, check pair correlation result

--- main.py
+++ main.py
@@ -1,41 +1,56 @@
 import numpy as np
 import sys
-
 import simulate as sim
 import plotting as plot
+import observables as obs
+import matplotlib.pyplot as plt
 
 # Input parameters
-N = 4*(5**3) # number of particles
-d = 3 # dimensionality of the box
-lattice_const = 1.54478 # lattice constant in units of sigma
-T = 2 # temperature in units of kB/epsilon
-num_tsteps = 900 # number of steps of the simulation
-run_time = 0.9 # run time of the simulation in units of sqrt(mass*sigma^2/epsilon)
-algorithm_method = "verlet" # method for numerical time evolution (options: "verlet" or "euler")
+particle_num = 4*(3**3) 
+dim = 3 
+lattice_const = 1.54478 # (sigma)
+
+temperature = 0.02 # (K*kB/epsilon)
+temperature_error = 0.005 # error in the temperature when rescaling (K*kB/epsilon)
 rescale_time = 0.1 # interval between rescalings
-T_error = 0.05 # error in the temperature wen rescaling
 
-# Velocity and Position initialization (uniformly random)
-init_pos, L = sim.fcc_lattice(N, lattice_const)
-init_vel = sim.init_velocity(N, T)
+run_time = 0.9 # sqrt(mass*sigma^2/epsilon)
+num_tsteps = 900 
+algorithm_method = "verlet" # options: "verlet" or "euler"
 
-# Run simulation
-print("Nparticles={}, lattice_const={}, box_dimension={}, temperature={}, run_time={}, num_tsteps={}, algorithm_method={}".format(N, lattice_const, L, T, run_time, num_tsteps, algorithm_method))
+# Velocity and Position initialization 
+init_pos, box_length = sim.fcc_lattice(particle_num, lattice_const)
+init_vel = sim.init_velocity(particle_num, temperature)
 
-# Get system to equilibrium
+# Simulate the system
+print("Nparticles={}, lattice_const={}, box_dimension={}, temperature={}, run_time={}, num_tsteps={}, algorithm_method={}".format(particle_num, lattice_const, box_length, temperature, run_time, num_tsteps, algorithm_method))
+
+# 1. Get system to equilibrium
 print("BRINGING SYSTEM TO EQUILIBRIUM...")
-eq_reached = sim.get_equilibrium(init_pos, init_vel, num_tsteps, run_time/num_tsteps, L, T, "output_eq.csv", method=algorithm_method, resc_thr=[T_error, rescale_time])
+eq_reached = sim.get_equilibrium(init_pos, init_vel, num_tsteps, run_time/num_tsteps, box_length, temperature, "output_eq.csv", method=algorithm_method, resc_thr=[temperature_error, rescale_time])
 if not eq_reached:
 	print("ERROR: equilibrium not reached")
 	sys.exit(0)
 print("DONE")
 
-# Run actual simulation under equilibrium
+# 2. Run simulation under equilibrium
 print("RUNNING SIMULATION UNDER EQUILIBRIUM...")
 eq_pos, eq_vel = sim.load_final_data("output_eq.csv")
-print("actual temperature = {:0.5f}".format(sim.temperature(eq_vel)))
-sim.simulate(eq_pos, eq_vel, num_tsteps, run_time/num_tsteps, L, T, "output.csv", method=algorithm_method)
+print("Temperature = {:0.5f}".format(sim.temperature(eq_vel)))
+sim.simulate(eq_pos, eq_vel, num_tsteps, run_time/num_tsteps, box_length, temperature, "output.csv", method=algorithm_method)
 print("DONE")
 
+# 3. Show results
+
+# Build a gif 
+#plot.GIF_3D("movie_FCClattice.gif", "output.csv", 100, box_length)
+
 # Check energy conservation
-plot.E_vs_t("output.csv", L, kinetic=True, potential=False, total=False)
+#plot.E_vs_t("output.csv", box_length, kinetic=True, potential=False, total=False)
+
+# Observables
+r, g = obs.pair_correlation_function("output.csv",0.01,box_length)
+
+plt.plot(r,g)
+plt.show()
+print( r[np.argmax(g[0:150])] ) # first maximum at sqrt(2)/2*lattice_constant = 1.0889
--- observables.py
+++ observables.py
@@ -3,7 +3,7 @@ import numpy as np
 import simulate as sim
 
 
-def pair_correlation_function(file_name, dr, L, r_max=None):
+def pair_correlation_function(file_name, dr, box_length, r_max=None):
     """
     Returns the pair correlation function averaged over time. 
 
@@ -13,7 +13,7 @@ def pair_correlation_function(file_name, dr, L, r_max=None):
         Name of the CSV file in which the data is stored
     dr : float
 		distance between bins in the histogram
-	L : float
+	box_length : float
 		Box size
     r_max : float
         Maximum value for r (distance between particles)
@@ -26,19 +26,19 @@ def pair_correlation_function(file_name, dr, L, r_max=None):
         Pair correlation function as a function of r
     """
 
-    if r_max is None: r_max = L
+    if r_max is None: r_max = box_length
     r = np.arange(dr, r_max+dr, dr) # start != 0, because g = 1/0 is not defined
     n = np.zeros(len(r))
     time, pos, _ = sim.load_data(file_name)
-    N = pos.shape[0]
+    particle_num = pos.shape[0]
 
     for k, t in enumerate(time):
         print("\r{}/{}".format(k+1, len(time)), end="")
-        rel_pos, rel_dist = sim.atomic_distances(pos[k], L)
+        rel_pos, rel_dist = sim.atomic_distances(pos[k], box_length)
         for i, r_ in enumerate(r):
             n[i] += len(np.where((rel_dist >= r_) & (rel_dist < r_ + dr))[0])
     
-    g = 2*L**3 / (N*(N-1)) * (n/len(time)) / (4*np.pi*r_**2 * dr)
+    g = 2*box_length**3 / (particle_num*(particle_num-1)) * (n/len(time)) / (4*np.pi*r_**2 * dr)
 
     print("")
 
@@ -63,13 +63,13 @@ def specific_heat(data_file,starting_time_step):
     time = time[starting_time_step:-1]
     pos = pos[starting_time_step:-1,:,:]
     vel = vel[starting_time_step:-1,:,:]
-    M = len(time) # Number of time steps
-    N = np.shape(vel)[1] # Number of particles
-    square_of_mean_kin = (sim.kinetic_energy(vel)/M)**2
-    mean_of_square_kin = (((0.5*(vel**2).sum(2)).sum(1))**2).sum()/M
+    num_tsteps = len(time) 
+    particle_num = np.shape(vel)[1] 
+    square_of_mean_kin = (sim.kinetic_energy(vel)/num_tsteps)**2
+    mean_of_square_kin = (((0.5*(vel**2).sum(2)).sum(1))**2).sum()/num_tsteps
     
     r = (mean_of_square_kin - square_of_mean_kin)/square_of_mean_kin # relative fluctuations in kinetic energy
 
-    c = 1.5/(1-3*N*r/2)
+    c = 1.5/(1-3*particle_num*r/2)
 
     return c
